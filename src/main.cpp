ab7f1f79126b26ddfae2cfb50682cb603ff645a9
Freddds <88401208+Freddds@users.noreply.github.com>
2026-04-16 19:43:57 +0200

    Replace template with LED blink example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
-#include <Arduino.h>
+/**
+ * Blink
+ *
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include "Arduino.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Set LED_BUILTIN if it is not defined by Arduino framework
+// #define LED_BUILTIN 13
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // wait for a second
+  delay(1000);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+
+   // wait for a second
+  delay(1000);
 }
